52532cce76b3b4a9cf500be9a1bcc677251a15a3
Bianca Teixeira <biancabt1102@gmail.com>
2023-10-26 16:03:44 -0300

    feat: add new files

---
+++ index.js
@@ -0,0 +1,6 @@
+//const chalk = require('chalk');
+import chalk from "chalk";
+
+console.log(chalk.blue('olá mundo'));
+console.log('São geralmente recuperados a partir de um objeto [FileList](https://developer.mozilla.org/pt-BR/docs/Web/API/FileList)');
+console.log('São geralmente recuperados a partir de um objeto [FileList](https://developer.mozilla.org/pt-BR/docs/Web/API/FileList)');
