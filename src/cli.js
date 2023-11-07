740434938902d009e56b25fe165f836e89ca406d
Bianca Teixeira <biancabt1102@gmail.com>
2023-11-07 15:57:00 -0300

    feat: config cli.js

--- src/cli.js
+++ src/cli.js
@@ -1,11 +1,49 @@
 import pegaArquivo from "./index.js";
 import chalk from "chalk";
+import fs from 'fs';
+import listaValidada from "./http-validacao.js";
 
 const caminho = process.argv;
 
-async function processaTexto(caminho){
-    const resultado = await pegaArquivo(caminho[2]);
-    console.log(chalk.yellow('lista de links'), resultado);
+function imprimeLista(valida, resultado, identificador = ""){
+
+    if(valida){
+        console.log(
+            chalk.yellow("lista validada"),
+            chalk.black.bgGreen(identificador),
+            listaValidada(resultado));
+    } else {
+        console.log(
+            chalk.yellow("lista de links"),
+            chalk.black.bgGreen(identificador),
+            resultado);
+    }
+}
+
+async function processaTexto(argumentos){
+    const caminho = argumentos[2];
+    const valida = argumentos[3] === '--valida';
+
+    try{
+        fs.lstatSync(caminho);
+    } catch (erro) {
+        if(erro.code === 'ENOENT'){
+            console.log("Arquivo ou diretório não existe");
+            return
+        }
+    }
+
+    if(fs.lstatSync(caminho).isFile()){
+        const resultado = await pegaArquivo(argumentos[2]);
+        imprimeLista(valida, resultado);
+
+    } else if(fs.lstatSync(caminho).isDirectory()){
+        const arquivos = await fs.promises.readdir(caminho);
+        arquivos.forEach(async (nomeDeArquivo) => {
+            const lista = await pegaArquivo(`${caminho}/${nomeDeArquivo}`);
+            imprimeLista(valida, lista, nomeDeArquivo);
+        })
+    }
 }
 
 processaTexto(caminho);
